57fe0426a4d25df343221a6707e13048f2a4d112
LaerteNF <laertenf@gmail.com>
2021-06-22 18:00:58 -0300

    fisrt use stage

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -10,8 +10,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <TestComponent/>
-          <ClassComponent/>
+          <TestComponent name="Laerte"/>
+          <ClassComponent name="Laerte"/>
         </p>
         <a
           className="App-link"
--- src/components/ClassComponent/ClassComponent.tsx
+++ src/components/ClassComponent/ClassComponent.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 
 class ClassComponent extends React.Component<{ name: String }> {
+
+    constructor(props: any){
+        super(props)
+        console.log('constructor reached')
+    }
+
     state = {
         name: 'Mundo!!!!'
     }
+
+    componentDidMount(){
+        console.log('componentDidMount reached')
+    }
+
+    componentDidUpdate(){
+        console.log('componentDidUpdate reached')
+    }
+
     render(){
+        console.log('render reached')
         return <div>
             <p>name: { this.state.name }</p>
             <button onClick={()=> {
