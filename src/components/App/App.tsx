d76fc791340def256650e8879e9f418443a3e319
LaerteNF <laertenf@gmail.com>
2021-06-23 12:47:54 -0300

    add button component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
+import Button from '../Button';
 import Header from '../Header';
 import './App.css';
+
+function TestComponent (){
+  return <img width="16"
+          src="https://cdn.icon-icons.com/icons2/561/PNG/512/active-search_icon-icons.com_53799.png" 
+          alt="search icon"
+        />
+}
+
 
 function App() {
   return (
     <div className="App">
       <Header title="Algastock" />
-      App
+      
+      <div className="Container">
+        <Button 
+          onClick={() => window.alert('UIIU')} 
+          appendIcon={<TestComponent />}
+        >
+          Alert
+        </Button>
+      </div>
+
     </div>
   );
 }
