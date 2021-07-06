cfd9b32909e18aebc8fdd053f6a25defa4170b80
LaerteNF <laertenf@gmail.com>
2021-07-06 15:17:36 -0300

    add rexus persist and blacklist products

--- src/redux/Products/Products.reducer.ts
+++ src/redux/Products/Products.reducer.ts
@@ -1,7 +1,7 @@
 import { Action } from ".."
-import Products, { Product } from "../../shared/Table/table.mockdata"
+import { Product } from "../../shared/Table/table.mockdata"
   
-  export default function (state = Products, action: Action): Product[]  {
+  export default function (state: Product[] = [], action: Action): Product[]  {
     switch (action.type) {
       case 'FETCH_PRODUCTS':
         return [...action.payload]
--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,13 +1,22 @@
 import { createStore, combineReducers, compose, applyMiddleware } from "redux";
 import Products from './Products/Products.reducer'
 import thunk, { ThunkAction} from 'redux-thunk'
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 const reducers = combineReducers({
     products: Products
 })
 
+const persistedReducer = persistReducer({
+    key: 'algastock',
+    storage,
+    // lista de reducers que não serão persistidos local
+    blacklist: ['products']
+}, reducers)
+
 const store = createStore(
-    reducers,
+    persistedReducer,
     compose(
         applyMiddleware(thunk),
         // @ts-ignore
@@ -15,6 +24,8 @@ const store = createStore(
     )
     
 )
+
+const persistor = persistStore(store)
 
 export interface Action<T = any> {
     type: string
@@ -28,4 +39,4 @@ export type Thunk<T = any> =
 
 export type ThunkDispatch = (thunk: Thunk) => Promise<Thunk>
 
-export default store
+export { store, persistor} 
